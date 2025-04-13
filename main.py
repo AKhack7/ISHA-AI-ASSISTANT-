944396697dab960c24c4fda51e77fe7221e60e3c
Ak_boy <amit635966@gmail.com>
2025-04-13 13:27:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-py
+import os
+import eel
+
+from engine.features import *
+from engine.command import *
+from engine.auth import recoganize
+def start():
+    
+    eel.init("www")
+
+    playAssistantSound()
+    
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html', mode=None, host='localhost', block=True)
+    
+    
